6eb11deb3d91e05864a4b225fc0140fda85a96db
Maycon Oliveira <mayconzayon@gmail.com>
2022-08-25 18:33:17 -0300

    exibindo na tela os valores atribuidos

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -4,3 +4,8 @@ let email = "joao@email.com.br"
 let profissao = "Professor"
 let idade = "30"
 let lugar = "Inglaterra"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
