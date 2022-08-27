bf032c2a85b418b6f978125df910c98d9423ea42
Maycon Oliveira <mayconzayon@gmail.com>
2022-08-26 22:49:17 -0300

    melhorando o código

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,7 +1,8 @@
-#!Preparando uma mensagem de uma agencia de turismo
 let nome = "Joao"
 let email = "joao@email.com.br"
 let profissao = "Professor"
 let idade = "30"
-let lugar = "Inglaterra"
-console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+let viagem = "Inglaterra"
+let mensagem = (`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagem}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+
+console.log(mensagem)
