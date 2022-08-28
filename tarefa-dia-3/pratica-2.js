9c6ee8c061166a183bc61b595b4b58e7c710e187
Maycon Oliveira <mayconzayon@gmail.com>
2022-08-28 03:29:11 -0300

    tabuada com while

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+//Tabuada de multiplicação de 7
+let multiplicador = 7
+let n = 10
+let i = 1
+while (i <= n) {
+    let resultado = multiplicador * i
+    let valorExibido = `${multiplicador} x ${i} = ${resultado}`
+    console.log (valorExibido)
+    i++
+}
