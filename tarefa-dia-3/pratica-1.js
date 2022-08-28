d9e368f2dbb435f00939fd7a579c0f069e712102
Maycon Oliveira <mayconzayon@gmail.com>
2022-08-28 02:55:15 -0300

    imprimindo números com while

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,17 @@
+//metódo 1
+let i = 1
+let n = 100
+
+while (i <= n) {
+    console.log(i)
+    i = i + 1    
+}
+
+//metódo 2
+let contador = 1;
+
+while (contador < 101) {
+  console.log(contador);
+
+  contador++;
+}
